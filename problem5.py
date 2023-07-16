91844d8bd0390ecfc427a43c7da8cccd44291f50
ThabisoGarikue <135240707+ThabisoGarikue@users.noreply.github.com>
2023-07-16 16:30:49 +0200

    Update problem5.py

--- problem5.py
+++ problem5.py
@@ -1,21 +1,19 @@
 def LCM():
-{
-		multiple = 20
-		sum=0
+    multiple = 20
+    sum = 0
 
-	while(sum==0):  
-	{
-		    if(multiple % 11 == 0 and multiple % 12 == 0 and multiple % 13 ==0 and multiple % 14 ==0
-		   and multiple % 15 ==0 and multiple % 16 ==0 and multiple % 17 ==0 and multiple % 18 ==0
-		   and multiple % 19 ==0 and multiple % 20 ==0):
-		   {
-			sum = multiple
-            }
-		     else
-		    {
-   			multiple = multiple + 20
-		    }
-		return sum
-	}
-}
+    while sum == 0:
+        found = True
+        for num in range(11, 21):
+            if multiple % num != 0:
+                found = False
+                break
+
+        if found:
+            sum = multiple
+        else:
+            multiple += 20
+
+    return sum
+
 print(LCM())
